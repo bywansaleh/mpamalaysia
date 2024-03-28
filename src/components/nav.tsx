d7d6c419d6128a6d79fd5f6628a78aa746c7a170
Wan Saleh <wansaleh@gmail.com>
2024-03-29 06:10:04 +0800

    Update backdrop filter in Nav component

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -7,16 +7,16 @@ export default function Nav() {
 	return (
 		<nav>
 			<div className="fixed inset-x-0 top-0 z-[999] flex h-20 items-center">
-				<div className="pointer-events-none absolute inset-x-0 top-0 z-0 h-28">
+				<div className="pointer-events-none absolute inset-x-0 top-0 z-0 h-24">
 					{Array.from({ length: 8 }).map((_, i) => {
 						return (
 							<div
 								key={i}
 								className="pointer-events-none absolute inset-0"
 								style={{
-									zIndex: i + 1,
-									WebkitBackdropFilter: `blur(${0.5 * (i + 1)}px)`,
-									backdropFilter: `blur(${0.5 * (i + 1)}px)`,
+									// zIndex: i + 1,
+									backdropFilter: `blur(${0.6 * (i + 1)}px)`,
+									WebkitBackdropFilter: `blur(${0.6 * (i + 1)}px)`,
 									mask: `linear-gradient(to top,
                     transparent ${12.5 * i}%,
                     black ${12.5 * (i + 1)}%,
