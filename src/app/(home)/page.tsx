41040da53c01fc09cc88648a2b0005a6db6d7b10
Wan Saleh <wansaleh@gmail.com>
2024-03-28 20:51:35 +0800

    Update bun.lockb, colors.css, dot.svg, pattern.svg, and page.tsx files

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,34 +2,18 @@
 
 // import img from '@/assets/images/mj1.png';
 
+import Companies from './sections/companies';
 import Council from './sections/council';
 import Pillars from './sections/pillars';
+import Welcome from './sections/welcome';
 
 export default function Home() {
 	return (
 		<>
-			<div className="relative">
-				<div className="container py-10 pt-32">
-					<div className="max-w-screen-md">
-						<h2 className="text-pretty font-heading text-4xl font-medium tracking-tight md:text-5xl">
-							<span className="text-muted-foreground/80">
-								Protecting and promoting
-							</span>{' '}
-							the rights and interests of music publishers in Malaysia.
-						</h2>
-						<p className="mt-40 text-pretty text-2xl font-light !leading-[1.1] md:text-3xl">
-							Music Publishers Association of Malaysia safeguards the rights and
-							promotes the works of music publishers in the country.{' '}
-							<span className="text-muted-foreground/80">
-								We focus on the following six pillars.
-							</span>
-						</p>
-					</div>
-				</div>
-			</div>
-
+			<Welcome />
 			<Pillars />
 			<Council />
+			<Companies />
 		</>
 	);
 }
---
+++ src/app/(home)/sections/companies.tsx
@@ -0,0 +1,56 @@
+export default function Companies() {
+	return (
+		<div className="relative overflow-hidden border-t [perspective:800px]">
+			<div className="absolute inset-0 z-0 bg-[url(/images/dot.svg)] bg-repeat opacity-30 invert [mask:linear-gradient(to_top,black,transparent)] [transform:rotateX(30deg)] dark:invert-0" />
+
+			<div className="container relative grid gap-6 py-10 md:grid-cols-2">
+				<h2 className="mb-8 text-pretty font-heading text-4xl tracking-tight md:text-5xl">
+					Publishing <span className="text-muted-foreground/80">Companies</span>
+				</h2>
+
+				<div className="">
+					{companies.map((company, i) => (
+						<div
+							key={i}
+							className="rounded-md px-2 py-1 hover:bg-brand hover:text-brand-foreground"
+						>
+							<h3>{company.name}</h3>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+type Company = {
+	name: string;
+};
+
+const companies: Company[] = [
+	{ name: 'Faithful Music Sdn Bhd' },
+	{ name: 'FMC Music Sdn Bhd' },
+	{ name: 'Frequen-Z Records' },
+	{ name: 'Goodsound Music Publishing Sdn Bhd' },
+	{ name: 'Indah Karya Publishing Sdn Bhd' },
+	{ name: 'Insichtech Musicland Sdn Bhd' },
+	{ name: 'Irama Bayu Production' },
+	{ name: 'Kelarai Sakti Publishing Sdn Bhd' },
+	{ name: 'KRU Music Sdn Bhd' },
+	{ name: 'Luncai Emas Sdn Bhd' },
+	{ name: 'Mixology Music Sdn Bhd' },
+	{ name: 'MVM Production Sdn Bhd' },
+	{ name: 'NAR Publishing Sdn Bhd' },
+	{ name: 'Nova Music (M) Sdn Bhd' },
+	{ name: 'NSR Music Publishing Sdn Bhd' },
+	{ name: 'Rock Records (M) Sdn Bhd' },
+	{ name: 'Rocketfuel Entertainment Sdn Bhd' },
+	{ name: 'RUSA Marketing Sdn Bhd' },
+	{ name: 'Star Media Publishing Sdn Bhd' },
+	{ name: 'Suwah Music Publishing Sdn Bhd' },
+	{ name: 'Taja Archive Sdn Bhd' },
+	{ name: 'Touch Music Publishing (M) Sdn Bhd' },
+	{ name: 'Universal Music Publishing Sdn Bhd' },
+	{ name: 'VEEDU Production Sdn Bhd' },
+	{ name: 'VMS Music Records & Publishing Sdn Bhd' },
+];
