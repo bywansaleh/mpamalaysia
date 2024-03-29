94efda1b7f39ed4b639fc35e1a901485f567b6f8
Wan Salehuddin <wansaleh@gmail.com>
2024-03-29 18:35:28 +0800

    Update Instagram section styling

--- src/app/(home)/sections/instagram.tsx
+++ src/app/(home)/sections/instagram.tsx
@@ -5,7 +5,7 @@ export default async function Instagram() {
 	const posts = await getInstagramPosts();
 
 	return (
-		<div className="relative overflow-hidden border-t">
+		<div className="relative overflow-hidden border-t bg-brand">
 			<div className="relative p-4">
 				{/* <h2 className="relative mb-12 text-pretty font-heading text-4xl tracking-tight md:text-5xl">
 					From <span className="text-muted-foreground">Instagram</span>
@@ -27,7 +27,7 @@ function Post({ post }: { post: Post }) {
 			rel="noopener noreferrer"
 			className={cn(
 				// styles.card,
-				'group relative flex flex-col gap-4 overflow-hidden rounded-md'
+				'group relative flex flex-col gap-4 overflow-hidden rounded-md bg-black text-white shadow-lg ring-2 ring-white'
 			)}
 		>
 			<img
@@ -35,21 +35,31 @@ function Post({ post }: { post: Post }) {
 				alt={post.caption}
 				className="aspect-square w-full object-cover transition-all duration-300 ease-in-out [mask:linear-gradient(to_bottom,black,rgba(0,0,0,0.3)_80%)] group-hover:scale-105 group-hover:opacity-60 group-hover:blur"
 			/>
-			<div className="absolute flex h-full flex-col justify-end gap-2 p-4">
-				<p className="line-clamp-6 translate-y-2 text-pretty text-2xs font-medium !leading-tight opacity-0 transition-all duration-300 ease-out-quad group-hover:translate-y-0 group-hover:opacity-100">
+			<div className="absolute inset-0 flex flex-col justify-end gap-2 p-2">
+				<p className="line-clamp-6 text-pretty text-2xs font-medium !leading-tight opacity-0 transition-all duration-100 group-hover:opacity-100">
 					{post.caption}
 				</p>
 				{/* <p className="text-pretty text-xs font-semibold">{post.username}</p> */}
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 256 256"
-					className="size-5"
-				>
-					<path
-						fill="currentColor"
-						d="M128 80a48 48 0 1 0 48 48a48.05 48.05 0 0 0-48-48m0 80a32 32 0 1 1 32-32a32 32 0 0 1-32 32m48-136H80a56.06 56.06 0 0 0-56 56v96a56.06 56.06 0 0 0 56 56h96a56.06 56.06 0 0 0 56-56V80a56.06 56.06 0 0 0-56-56m40 152a40 40 0 0 1-40 40H80a40 40 0 0 1-40-40V80a40 40 0 0 1 40-40h96a40 40 0 0 1 40 40ZM192 76a12 12 0 1 1-12-12a12 12 0 0 1 12 12"
-					/>
-				</svg>
+				<div className="flex w-full flex-row-reverse items-center justify-between gap-1 text-2xs font-semibold tracking-tighter">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 256 256"
+						className="size-5"
+					>
+						<path
+							fill="currentColor"
+							d="M128 80a48 48 0 1 0 48 48a48.05 48.05 0 0 0-48-48m0 80a32 32 0 1 1 32-32a32 32 0 0 1-32 32m48-136H80a56.06 56.06 0 0 0-56 56v96a56.06 56.06 0 0 0 56 56h96a56.06 56.06 0 0 0 56-56V80a56.06 56.06 0 0 0-56-56m40 152a40 40 0 0 1-40 40H80a40 40 0 0 1-40-40V80a40 40 0 0 1 40-40h96a40 40 0 0 1 40 40ZM192 76a12 12 0 1 1-12-12a12 12 0 0 1 12 12"
+						/>
+					</svg>
+
+					<span className="opacity-0 transition-all duration-100 group-hover:opacity-100">
+						{new Intl.DateTimeFormat('en-GB', {
+							dateStyle: 'medium',
+							// timeStyle: 'short',
+							timeZone: 'Asia/Kuala_Lumpur',
+						}).format(new Date(post.timestamp))}
+					</span>
+				</div>
 			</div>
 		</a>
 	);
