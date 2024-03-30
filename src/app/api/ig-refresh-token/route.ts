c94a40b56c5c32c8536edeff2435e36e9bc918f7
Wan Salehuddin <wansaleh@gmail.com>
2024-03-31 06:49:22 +0800

    Delete IG refresh token and IG route

--- src/app/api/ig-refresh-token/route.ts
+++
@@ -1,9 +0,0 @@
-export const runtime = 'edge';
-
-export async function GET() {
-	const result = await fetch(
-		`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${process.env.IG_ACCESS_TOKEN}`
-	).then((res) => res.text());
-
-	return Response.json(result);
-}
--- src/app/api/ig/route.ts
+++
@@ -1,9 +0,0 @@
-export const runtime = 'edge';
-
-export async function GET() {
-	const result = await fetch(
-		`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink,timestamp,username&access_token=${process.env.IG_ACCESS_TOKEN}`
-	).then((res) => res.json());
-
-	return Response.json(result);
-}
