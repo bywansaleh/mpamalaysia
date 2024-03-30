08947694a1bc6a64df45a7451fbe294c02db1d9b
Wan Salehuddin <wansaleh@gmail.com>
2024-03-31 07:11:13 +0800

    Update dynamic and revalidate constants

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,9 +7,9 @@ import Instagram from './sections/instagram';
 import Pillars from './sections/pillars';
 import Welcome from './sections/welcome';
 
-// export const dynamic = 'force-static';
-// export const revalidate = 60;
-export const runtime = 'edge';
+export const dynamic = 'force-static';
+export const revalidate = 60;
+// export const runtime = 'edge';
 
 export default function Home() {
 	return (
