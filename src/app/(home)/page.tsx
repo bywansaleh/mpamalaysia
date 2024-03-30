64edb4a58fc62e1e4b468167262723237bd09bdc
Wan Salehuddin <wansaleh@gmail.com>
2024-03-31 07:03:11 +0800

    Remove unused imports and commented code

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 
-// import img from '@/assets/images/mj1.png';
-
 import Achievements from './sections/achievements';
 import Companies from './sections/companies';
 import Council from './sections/council';
@@ -9,8 +7,8 @@ import Instagram from './sections/instagram';
 import Pillars from './sections/pillars';
 import Welcome from './sections/welcome';
 
-export const dynamic = 'force-static';
-export const revalidate = 60;
+// export const dynamic = 'force-static';
+// export const revalidate = 60;
 export const runtime = 'edge';
 
 export default function Home() {
