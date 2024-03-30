b576ae7a2a9a5bcfdf68f85a5a4a1e8c0e8cbba2
Wan Salehuddin <wansaleh@gmail.com>
2024-03-31 06:45:44 +0800

    Remove unused dependencies and update global CSS

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -11,6 +11,7 @@ import Welcome from './sections/welcome';
 
 export const dynamic = 'force-static';
 export const revalidate = 60;
+export const runtime = 'edge';
 
 export default function Home() {
 	return (
