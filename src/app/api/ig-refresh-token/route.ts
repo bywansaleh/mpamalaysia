b0fbe1aa1e0f9040de5221430e2d6640ccdfd9e6
Wan Salehuddin <wansaleh@gmail.com>
2024-03-31 06:42:43 +0800

    Add runtime variable to ig-refresh-token and ig routes

--- src/app/api/ig-refresh-token/route.ts
+++ src/app/api/ig-refresh-token/route.ts
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 export async function GET() {
 	const result = await fetch(
 		`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${process.env.IG_ACCESS_TOKEN}`
--- src/app/api/ig/route.ts
+++ src/app/api/ig/route.ts
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 export async function GET() {
 	const result = await fetch(
 		`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink,timestamp,username&access_token=${process.env.IG_ACCESS_TOKEN}`
