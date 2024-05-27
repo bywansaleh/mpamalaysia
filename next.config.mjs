c5517917778daab2ed9fe1283b35d425100e40fc
Wan Salehuddin <wansaleh@gmail.com>
2024-05-27 20:50:04 +0800

    Update @tabler/icons-react and framer-motion dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	swcMinify: true,
-	// experimental: {
-	// 	ppr: true,
-	// },
+	// swcMinify: true,
+	experimental: {
+		// ppr: true,
+		reactCompiler: true,
+	},
 };
 
 export default nextConfig;
